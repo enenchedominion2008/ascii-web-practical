050f223a30f2ac1dd211af157a75385a667f7f91
enenchedominion2008 <enenchedominion@gmail.com>
2026-07-06 16:17:01 +0100

    i have done my main.go but am not throgh yet

--- mytest.go/domi/main.go.go
+++ mytest.go/domi/main.go.go
@@ -22,4 +22,22 @@ func main(){
 	fmt.Println("color:",colorName)
 	fmt.Println("substring:",substring)
 	fmt.Println("text:",text)
+	color := map[string]string{
+		"red" : "\033[31m",
+		"green" : "\033[32m",
+		"yellow" : "\033[33m",
+		"blue" : "\033[34m",
+		"purple" : "\033[35m",
+		"skyblue" : "\033[36m",
+		"white" : "\033[37m",
+
+	}
+	ansiCode ,exist := color[colorName] 
+	if !exist {
+		fmt.Println("error: color \""+colorName+"\"is not surported")
+		return
+	}
+	fmt.Println("ANSI code:",ansiCode)
+	result := ascii(text, "standard.txt")
+fmt.Print(result)
 }
